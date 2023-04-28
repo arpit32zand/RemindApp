2d55bacab3b0b72c3dd5e12c8fec78c4bd654a90
shubhangisrivastava@09 <shubahngi.sri09@gmail.com>
2023-04-28 21:48:37 +0530

    SetReminder

---
+++ src/Screens/HomePage.js
@@ -0,0 +1,105 @@
+import React, { useState } from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+// import { Navigate } from "react-router-dom";
+import homeBackground from "../Assets/background.jpg";
+
+const name="Shubhangi Srivastava"
+const current=new Date()
+const currentDate= new Date(current).toLocaleDateString();
+const date=`${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
+
+export default function HomePage(){
+ 
+    const styles={
+        root: {
+            // height: "100vh",
+            backgroundImage: `url(${homeBackground})`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            color: 'white'
+        },
+        innerContainer: {
+            width: "100%",
+            height: '80vh',
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: 'space-between',
+            alignItems:"center",
+            padding: '2% 2% 0',
+        },
+        button:{
+            width: '250px',
+            height:'50px',
+            backgroundColor :"#ff8080",
+            color:"#000000",
+            fontWeight:"bold",
+        },
+        backStyle:{
+            backgroundColor: 'rgba(0, 0, 0, 0.7)',
+            height:"100vh",
+        },
+        headings:{
+            display: 'flex',
+            flexDirection: 'column',
+            justityContent: 'center',
+            alignItems: 'center'
+        }
+    }
+    return(
+        <Grid style={styles.root}>
+            <CssBaseline/>
+            <div style={styles.backStyle} >
+                <Grid style={styles.headings}>
+                    <Typography component="h1" variant="h4" style={styles.welcomeHead}>
+                        Welcome to the Reminder Application {name}
+                    </Typography>
+                    <Typography component="h1" variant="h5"style={styles.dateHead}  >
+                        Today is {date}
+                    </Typography>
+                </Grid>
+                <div style={styles.innerContainer}>
+                    <Button 
+                        style={styles.button} 
+                        variant="contained"
+                    >
+                        Set Reminder
+                    </Button>
+                    <Button 
+                        style={styles.button} 
+                        variant="contained"
+                    >
+                        Modify Reminder
+                    </Button>
+                    <Button 
+                        style={styles.button} 
+                        variant="contained"
+                    >
+                        Disable Reminder
+                    </Button>
+                    <Button 
+                        style={styles.button} 
+                        variant="contained"
+                    >
+                        Delete Reminder
+                    </Button>
+                    <Button 
+                        style={styles.button} 
+                        variant="contained"
+                    >
+                        Enable Reminder
+                    </Button>
+                    <Button 
+                        style={styles.button} 
+                        variant="contained"
+                    >
+                        View Reminder
+                    </Button>
+                </div>
+            </div>
+        </Grid>
+    )
+}
